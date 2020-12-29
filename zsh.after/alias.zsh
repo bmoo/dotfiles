caa3ff115fa232ca6c5f4effba6c1eed57ca70e6
Brad Moore <brad.moore@arcules.com>
2020-12-29 08:45:29 -0800

    Alias kubectl as k

--- zsh.after/alias.zsh
+++ zsh.after/alias.zsh
@@ -1,5 +1,5 @@
 alias dk=docker-compose
-alias ks=kubectl
+alias k=kubectl
 alias dlogin='$(aws-vault exec --no-session verve -- aws ecr get-login --no-include-email --region us-west-2)'
 alias couchshell='docker run --rm -ti --net=otas_default couchbase/server:4.6.3 /opt/couchbase/bin/cbq -u vagrant -p vagrant -engine=http://couchbase:8091/'
 alias pg='PGPASSWORD=secret docker run -it --rm --network otas_default postgres psql -h postgres -U postgres'
@@ -8,3 +8,4 @@ alias geti='getifunc() { ssh puppet-east ./get-instance.sh $1 }; getifunc'
 alias gotest='go test -count=1 -short ./...'
 alias gitfilehist='gitlogfunc() { git log --format=%H -- $1 | head -n1}; gitlogfunc'
 alias lint='golangci-lint run'
+
