2887a6c84ef47895311c8614fdbccfb04df29bdb
Brad Moore <867732+bmoo@users.noreply.github.com>
2023-02-13 09:54:32 -0800

    Configure treesitter for go files

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -1,6 +1,13 @@
 -- tree sitter config
 require("nvim-treesitter.configs").setup({
+    ensure_installed = { "lua", "vim", "help", "go" },
     highlight = {
+        enable = true,
+    },
+    incremental_selection = {
+        enable = true,
+    },
+    indent = {
         enable = true,
     },
 })
